d3f129630566525aeb755f82abef0c642577f0a6
BL-Jotiram <jotiram.bandgar@bridgelabz.com>
2021-03-27 11:05:16 +0530

    Added: Functionality to create employee in address book

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.addressbook;
 
 public class AddressBookMain {
+    Contact contact;
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+    }
+
+    public boolean createContact(Contact contact) {
+        this.contact = contact;
+        if(this.contact == null){
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/test/java/AddressBookTest.java
@@ -0,0 +1,23 @@
+import com.bridgelabz.addressbook.AddressBookMain;
+import com.bridgelabz.addressbook.Contact;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddressBookTest {
+
+    @Test
+    public void givenContact_WhenProper_ShouldReturnTrue() {
+        Contact contact = new Contact("Abcd","AAA","0000000000");
+        AddressBookMain addressBookMain = new AddressBookMain();
+        boolean contactStatus = addressBookMain.createContact(contact);
+        Assert.assertEquals(true, contactStatus);
+    }
+
+    @Test
+    public void givenNullContact_WhenCreated_ShouldReturnFalse() {
+        Contact contact = null;
+        AddressBookMain addressBookMain = new AddressBookMain();
+        boolean contactStatus = addressBookMain.createContact(contact);
+        Assert.assertEquals(false, contactStatus);
+    }
+}
